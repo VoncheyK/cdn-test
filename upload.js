0dc2edda4df8cfbdaef5809e0211b367a805a897
Vonchyy <VoncheyK@gmail.com>
2023-02-14 19:02:05 +0200

    added some changes to my repo!

--- upload.js
+++ upload.js
@@ -1,17 +1,37 @@
-function uploadFile() {
-    var form = document.getElementById("uploadForm");
-    var fileInput = document.getElementById("fileToUpload");
-    var file = fileInput.files[0];
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/.netlify/functions/upload");
-    xhr.setRequestHeader("Content-Type", file.type);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        document.getElementById("uploadStatus").innerHTML = "File uploaded successfully!";
-      } else {
-        document.getElementById("uploadStatus").innerHTML = "Error uploading file.";
-      }
-    };
-    xhr.send(file);
+const form = document.querySelector('form');
+const fileInput = document.querySelector('#file-input');
+const submitBtn = document.querySelector('#submit-btn');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const file = fileInput.files[0];
+  const filename = `f/${Math.floor(Math.random() * 100000) + 1}${file.name.substring(file.name.lastIndexOf('.'))}`;
+  const formData = new FormData();
+  formData.append('file', file, filename);
+
+  const response = await fetch('/.netlify/functions/upload', {
+    method: 'POST',
+    body: formData,
+  });
+
+  if (response.ok) {
+    window.open(`/${filename}`, '_blank');
   }
-  
+});
+
+// Netlify function to handle file upload
+const { create } = require('@netlify/functions');
+const fs = require('fs');
+
+exports.handler = create(async (event) => {
+  const { file, filename } = event.body;
+
+  const fileBuffer = Buffer.from(file, 'base64');
+  fs.writeFileSync(`public/${filename}`, fileBuffer);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success: true }),
+  };
+});
